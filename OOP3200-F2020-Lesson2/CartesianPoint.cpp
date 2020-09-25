9f9b92a3a4981d54da4d1be79ef7c95d7cf4070f
Lau Nelson <smartsonlau1997@gmail.com>
2020-09-24 23:43:38 -0400

    added operator>> and operator<< to CartesianPoint

---
+++ OOP3200-F2020-Lesson2/CartesianPoint.cpp
@@ -0,0 +1,124 @@
+#include "CartesianPoint.h"
+
+#include <sstream> 			// stringstream
+#include <cmath>			// sqrt()
+
+// Class definition section
+
+// constructor for CartesianPoint
+CartesianPoint::CartesianPoint(const int x, const int y): m_x(x), m_y(y)
+{
+	//SetPoint(x, y);
+}
+
+CartesianPoint::~CartesianPoint()
+= default;
+
+CartesianPoint::CartesianPoint(const CartesianPoint& point2)
+{
+	SetPoint(point2.GetX(), point2.GetY());
+}
+CartesianPoint CartesianPoint::operator+(const CartesianPoint& point2) const
+{
+	CartesianPoint tempPoint;
+	tempPoint.SetX((*this).GetX() + point2.GetX());
+	tempPoint.SetY((*this).GetY() + point2.GetY());
+
+	return tempPoint;
+}
+
+double CartesianPoint::operator-(const CartesianPoint& point_to) const
+{
+	// difference between x values
+	const int xDelta = point_to.GetX() - m_x;
+
+	// difference between y values
+	const int yDelta = point_to.GetY() - m_y;
+
+	// return the formula (based on Pythagorean theorem)
+	return sqrt((xDelta * xDelta) + (yDelta * yDelta));
+}
+
+bool CartesianPoint::operator==(const CartesianPoint& other_point) const
+{
+	return ((GetX() == other_point.GetX()) && (GetY() == other_point.GetY()));
+}
+
+std::ostream& operator<<(std::ostream& out, const CartesianPoint& point)
+{
+	out << point.ToString();
+	return out;
+}
+
+
+std::istream& operator>>(std::istream& in, CartesianPoint& point)
+{
+	in >> point.m_x;
+	in.ignore();
+	in >> point.m_y;
+
+	return in;
+}
+
+
+void CartesianPoint::SetPoint(int x, int y)
+{
+	SetX(x);
+	SetY(y);
+}
+
+void CartesianPoint::SetX(int x)
+{
+	m_x = x;
+}
+
+void CartesianPoint::SetY(int y)
+{
+	m_y = y;
+}
+
+int CartesianPoint::GetX() const
+{
+	return m_x;
+}
+
+int CartesianPoint::GetY() const
+{
+	return m_y;
+}
+
+
+/** GetDistanceTo Method for CartesianPoint class
+*	Determines the distance between this point and a second point.
+*	@param	pointTo: CartesianPoint
+*	@return	the distance as a double
+*/
+double CartesianPoint::GetDistanceTo(const CartesianPoint& point_to) const
+{
+	// difference between x values
+	const int xDelta = point_to.GetX() - m_x;
+
+	// difference between y values
+	const int yDelta = point_to.GetY() - m_y;
+
+	// return the formula (based on Pythagorean theorem)
+	return sqrt((xDelta * xDelta) + (yDelta * yDelta));
+}
+
+/** ToString Method for CartesianPoint class
+*	Converts the obj to a string.
+*	@return	the obj state as a string
+*/
+
+std::string CartesianPoint::ToString() const
+{
+	// declare a stringstream object
+	std::stringstream strOut;
+
+	// build the string
+	strOut << "(" << m_x << ", " << m_y << ")";
+
+	// return the string
+	return strOut.str();
+}
+
---
+++ OOP3200-F2020-Lesson2/CartesianPoint.h
@@ -0,0 +1,68 @@
+#pragma once
+#ifndef __CARTESIAN_POINT__
+#define __CARTESIAN_POINT__
+
+#include <string>
+
+// class declaration section
+class CartesianPoint
+{
+public:
+
+	/* Constructor: Used to initialize objects*/
+	CartesianPoint(int x = 1, int y = 1);
+	~CartesianPoint();
+	
+	/* Copy Constructor */
+	CartesianPoint(const CartesianPoint& point2);
+
+	/*operator overloads*/
+
+	CartesianPoint operator+(const CartesianPoint& point2) const;
+
+	double operator-(const CartesianPoint& point_to) const;
+	bool operator==(const CartesianPoint& other_point) const;
+
+	/* friend overloads */
+	friend std::ostream& operator<<(std::ostream& out, CartesianPoint point);
+	friend std::istream& operator>>(std::istream& in, CartesianPoint& point);
+	// --------------------------------------------------------------------------------
+	/* Accessors: Used to query the state of the object*/
+
+	// get x
+	int GetX() const;
+
+	// get y
+	int GetY() const;
+
+	// -------------------------------------------------------------------------------
+	/* Mutator(s): Used to change the state of the object
+	*	- should contain logic to ensure object remains in a valid state.
+	*	- typically sets a member variable to a parameter
+	*/
+
+	// set x
+	void SetX(int x);
+
+	// set y,
+	void SetY(int y);
+
+	// set point (both x and y)
+	void SetPoint(int x, int y);
+
+
+	// get the distance between this point and a second point
+	double GetDistanceTo(const CartesianPoint& point_to) const;
+
+	// convert the obj to a string
+	std::string ToString() const;
+
+
+private:
+	// private data members for the dimensions of the point
+	int m_x; // x-axis (horizontal) value
+	int m_y;  // y-axis (vertical) value
+
+};
+
+#endif /* defined (__CARTESIAN_POINT);*/
